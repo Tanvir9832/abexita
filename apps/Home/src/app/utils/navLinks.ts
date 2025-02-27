75f4ab1b2fb4b797a3edc0493f7c093e77ef43f4
tanviAlam <tanvir9832@gmail.com>
2025-02-27 13:58:49 +0600

    initiate

---
+++ apps/Home/src/app/utils/navLinks.ts
@@ -0,0 +1,42 @@
+const NAV_LINKS = [
+    {
+      id: 1,
+      page: "Features",
+      href: "/",
+      children: []
+    },
+    {
+      id: 1,
+      page: "Features",
+      href: "/",
+      children: []
+    },
+    {
+      id: 2,
+      page: "Pricing",
+      href: "/"
+    },
+    {
+      id: 3,
+      page: "Solutions",
+      href: "/",
+      children: []
+    },
+    {
+      id: 4,
+      page: "Resources",
+      href: "/",
+      children: []
+    },
+    {
+      id: 5,
+      page: "Find Professionals",
+      href: "/"
+    },
+    {
+      id: 6,
+      page: "Help",
+      href: "/",
+      children: []
+    }
+  ]
